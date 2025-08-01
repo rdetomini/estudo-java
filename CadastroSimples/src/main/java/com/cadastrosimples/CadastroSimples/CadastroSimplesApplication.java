98524d50003c2130bfe089d34694f3d50e044248
Rodrigo Detomini <rdetomini@hotmail.com>
2025-08-01 20:38:38 -0300

    Configuração spring

---
+++ CadastroSimples/src/main/java/com/cadastrosimples/CadastroSimples/CadastroSimplesApplication.java
@@ -0,0 +1,13 @@
+package com.cadastrosimples.CadastroSimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroSimplesApplication.class, args);
+	}
+
+}
---
+++ CadastroSimples/src/test/java/com/cadastrosimples/CadastroSimples/CadastroSimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastrosimples.CadastroSimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroSimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
