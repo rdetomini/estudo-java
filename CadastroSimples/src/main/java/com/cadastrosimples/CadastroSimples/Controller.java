65f8715c9576616483de8d4bccb89314d03ac399
Rodrigo Detomini <rdetomini@hotmail.com>
2025-08-01 21:22:19 -0300

    Criação controller e rota

---
+++ CadastroSimples/src/main/java/com/cadastrosimples/CadastroSimples/Controller.java
@@ -0,0 +1,15 @@
+package com.cadastrosimples.CadastroSimples;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Hello world de rota";
+    }
+}
