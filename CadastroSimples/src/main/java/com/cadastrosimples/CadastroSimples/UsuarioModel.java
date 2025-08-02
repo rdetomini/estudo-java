d21fea1445db247b09697eb02f5f1593dee91fb6
Rodrigo Detomini <rdetomini@hotmail.com>
2025-08-02 10:11:19 -0300

    Adicionado dependencia, criação model e configuração entidade

---
+++ CadastroSimples/src/main/java/com/cadastrosimples/CadastroSimples/UsuarioModel.java
@@ -0,0 +1,50 @@
+package com.cadastrosimples.CadastroSimples;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class UsuarioModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+    public UsuarioModel() {
+    }
+    public UsuarioModel(String email, int idade, String nome) {
+        this.email = email;
+        this.idade = idade;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
